9dea5a03864c9fe54b2d5c2ad9529f4d7de77c06
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2018-11-10 16:36:45 +0100

    Added walls / ceiling to scene

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ var colors = {
 	darkred : 0x5e0004,
 	darkgreen : 0x0e3000,
 	green : 0x1a5900,
-	lightgreen: 0x34b200
+	lightgreen: 0x529b33,
+	brown: 0x3f1b00
 	};
 				
 //Setup Renderer
@@ -20,20 +21,39 @@ var scene = new THREE.Scene();
 
 //Set camera setting
 var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(0,25,100);
-//camera.lookAt(0,0,0);
+camera.position.set(15,25,100);
+camera.lookAt(0,0,0);
 
 //Create Lights
 createAmbientLight(colors.white);
-var pointLight = createPointLight(colors.white);
-pointLight.position.set(0,20,20);
-
+var light = createPointLight(colors.white);
+light.position.set(0,5,30);
+/*pointLight.castShadow = true;*/
 
 //Create Objects
-/*var p = createPlane(100,100,0, colors.yellow);
-p.rotation.x = 1.57*/
+var p = createPlane(100,140,0, colors.brown);
+p.rotation.x = Deg2Rad(90);
 
-createTree();
+p = createPlane(100,100,0, colors.brown);
+p.rotation.x = Deg2Rad(90);
+p.position.y = 40;
+
+p = createPlane(100,40,0, colors.brown);
+p.position.set(0,20,-50);
+
+p = createPlane(100,40,0,colors.brown);
+p.rotation.y = Deg2Rad(90);
+p.position.set(50,20,0);
+
+p = createPlane(100,40,0,colors.brown);
+p.rotation.y = Deg2Rad(90);
+p.position.set(-50,20,0);
+
+
+
+
+var tree = createTree();
+tree.position.y = 7;
 
 //Render loop
 animate();
@@ -70,15 +90,17 @@ function createTetrahedron(color,radius,subdiv=0, flatShading=false)
 	geo.applyMatrix( new THREE.Matrix4().makeRotationAxis( new THREE.Vector3( 1, 0, -1 ).normalize(), Math.atan( Math.sqrt(2)) ) );
 	let tetra = new THREE.Mesh(geo,mat);
 	tetra.rotation.y = -5;
-	
+	tetra.receiveShadow = true;
+
 	return tetra;
 }
 
 function createPlane(x=0,y=0,z=0,color=colors.white)
 {
-	mat = new THREE.MeshPhongMaterial({color: color, side: THREE.DoubleSide});
-	geo = new THREE.PlaneGeometry(x,y,z);
+	let mat = new THREE.MeshPhongMaterial({color: color, side: THREE.DoubleSide, shininess: 2});
+	let geo = new THREE.PlaneGeometry(x,y,z);
 	let plane = new THREE.Mesh(geo,mat);
+	
 	scene.add(plane);
 	return plane;
 }
@@ -93,15 +115,13 @@ function createCylinder(radiusTop, radiusBot, height, segments, color,flatShadin
 }
 
 
-
 function createTree()
 {
 	let treeGroup = new THREE.Group();
 
-
 	//Tree Trunk
-	let trunk = createCylinder(3,3,10,5,colors.darkgreen);
-	trunk.position.x = 10;
+	let trunk = createCylinder(2,2,6,6,colors.brown,true);
+	trunk.position.y = -6;
 
 	//Bottom branches
 	let bot = createTetrahedron(colors.darkgreen, 10);
@@ -117,9 +137,9 @@ function createTree()
 	mid2.position.y = 5;
 	
 	//Top Branches
-	let top = createTetrahedron(0x529b33, 7);
+	let top = createTetrahedron(colors.lightgreen, 7);
 	top.position.y = 10;
-	let top2 = createTetrahedron(0x529b33, 7);
+	let top2 = createTetrahedron(colors.lightgreen, 7);
 	top2.position.y = 10;
 	top2.rotation.y = 4.5;
 
@@ -127,4 +147,11 @@ function createTree()
 	scene.add(treeGroup);
 
 	return treeGroup;
+}
+
+
+
+function Deg2Rad(degrees)
+{
+	return degrees * (Math.PI / 180);
 }
