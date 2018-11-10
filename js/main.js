b9ad0a98368c2450bdde73caf036eb567d3385a7
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2018-11-10 14:41:19 +0100

    added some comments and possibility to enable flatShading

--- js/main.js
+++ js/main.js
@@ -63,9 +63,9 @@ function createPointLight(color)
 	return pointlight;
 }
 
-function createTetrahedron(color,radius,subdiv=0)
+function createTetrahedron(color,radius,subdiv=0, flatShading=false)
 {
-	let mat = new THREE.MeshPhongMaterial({color: color});
+	let mat = new THREE.MeshPhongMaterial({color: color, flatShading: flatShading});
 	let geo = new THREE.TetrahedronGeometry(radius,subdiv);
 	geo.applyMatrix( new THREE.Matrix4().makeRotationAxis( new THREE.Vector3( 1, 0, -1 ).normalize(), Math.atan( Math.sqrt(2)) ) );
 	let tetra = new THREE.Mesh(geo,mat);
@@ -83,29 +83,47 @@ function createPlane(x=0,y=0,z=0,color=colors.white)
 	return plane;
 }
 
+function createCylinder(radiusTop, radiusBot, height, segments, color,flatShading=false)
+{
+	var geo = new THREE.CylinderGeometry(radiusTop, radiusBot, height, segments);
+	var mat = new THREE.MeshPhongMaterial({color: color, flatShading: flatShading})
+	var cylinder = new THREE.Mesh(geo,mat);
+
+	return cylinder;
+}
+
+
+
 function createTree()
 {
 	let treeGroup = new THREE.Group();
 
+
+	//Tree Trunk
+	let trunk = createCylinder(3,3,10,5,colors.darkgreen);
+	trunk.position.x = 10;
+
+	//Bottom branches
 	let bot = createTetrahedron(colors.darkgreen, 10);
 	let bot2 = createTetrahedron(colors.darkgreen, 9);
 	bot2.rotation.y = 4.5;
 	
 
+	//Mid Branches
 	let mid = createTetrahedron(colors.green, 9);
 	mid.position.y = 5;
 	let mid2 = createTetrahedron(colors.green, 8);
 	mid2.rotation.y = 4.5;
 	mid2.position.y = 5;
 	
-
+	//Top Branches
 	let top = createTetrahedron(0x529b33, 7);
 	top.position.y = 10;
 	let top2 = createTetrahedron(0x529b33, 7);
 	top2.position.y = 10;
 	top2.rotation.y = 4.5;
 
-	treeGroup.add(bot,bot2,mid,mid2,top, top2);
+	treeGroup.add(trunk,bot,bot2,mid,mid2,top, top2);
 	scene.add(treeGroup);
 
 	return treeGroup;
